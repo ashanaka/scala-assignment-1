3c66a8c8ae6f0332639cd37e9dcee112194ceb42
Ashan Shanaka <jpashanshanaka@gmail.com>
2020-05-19 12:16:52 +0530

    1 done

--- Assignment.scala
+++ Assignment.scala
@@ -1,5 +1,11 @@
 object Assignment{  
-    def main(args:Array[String]){  
-        println ("Hello Scala")  
-    }  
+    def main(args: Array[String]) = {  
+        convert(35.0) 
+    }
+
+    def convert(deg:Double) = {
+		var far:Double = 0
+		far = deg*1.8000 + 32.00 
+		printf(deg.toString + " degrees is equal to " + far.toString + " F")
+    } 
 }

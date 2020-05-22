1d37e9cf7301ef863517080e8a18184e2dea01be
Ashan Shanaka <jpashanshanaka@gmail.com>
2020-05-22 17:56:00 +0530

    assignment-2-problem-1

--- assignment-2/assignment2.scala
+++ assignment-2/assignment2.scala
@@ -1,13 +1,13 @@
 object Assignment2{  
     def main(args: Array[String]) = { 
-    	xyzCompany() 
+    	
+        printf(xyzCompany(40, 20).toString + "/=\n")
     }
 
     //problem-1
-    def xyzCompany() = {
-		var total:Double = 0
-        total = normalSalary(40) + otSalary(20) - tax(normalSalary(40)) - tax(otSalary(20))
-        printf("Total Salary " + total + "/=\n")
+    def xyzCompany(nHours:Double, otHours:Double) = {
+		normalSalary(nHours) + otSalary(otHours) - tax(normalSalary(nHours)) - tax(otSalary(otHours))
+        
     }
     def normalSalary(hours:Double):Double = {
         hours*150.0

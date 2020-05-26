18b7bd987aef2c9bc6af0831f5290a226e879f92
Ashan Shanaka <jpashanshanaka@gmail.com>
2020-05-27 01:31:47 +0530

    assignment-3

--- assignment-3/Assignment3.scala
+++ assignment-3/Assignment3.scala
@@ -1,7 +1,11 @@
 object Assignment3{
     def main(args: Array[String]):Unit = {
+        fibonacciSeq(10)
+        println(addAllEven(10))
+        println(isEven(9))
+        println(addition(5))
         primeSeq(20) 
-        // prime(1)
+        prime(3)
     }
 
     //problem-1
@@ -23,5 +27,36 @@ object Assignment3{
         if(n == 2)  0
         if(n>2) primeSeq(n-1)
     }
+
+    //problem-3
+    def addition(n:Int):Int = n match {
+        case 1 => 1
+        case _ => n + addition(n-1)
+    }
+
+    //problem-4
+    def isEven(n:Int):Boolean = n match {
+        case 0 => true
+        case _ => isOdd(n-1)
+    }
+    def isOdd(n:Int):Boolean = !(isEven(n))
+
+    //problem-5
+    def addAllEven(n:Int):Int = n match {
+        case x if(x==0) => 0
+        case x if(isEven(x)) => n + addAllEven(n-1)
+        case x => addAllEven(n-1)
+    }
+
+    //problem-6
+    def fibonacci(n:Int):Int = n match{
+        case 0 => 0
+        case x if x==1 => 1
+        case _ => fibonacci(n-1) + fibonacci(n-2)
+    }
+    def fibonacciSeq(n:Int):Unit = {
+        if(n>0) fibonacciSeq(n-1)
+        println(fibonacci(n))
+    }
 }
-// Assignment3.main(args)
+

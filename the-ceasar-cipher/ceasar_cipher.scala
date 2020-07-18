45ae6d8f2ccbb10cf7a941104e819dd7dc32dbef
Ashan Shanaka <jpashanshanaka@gmail.com>
2020-07-18 23:36:51 +0530

    ceasar cipher assignment completed

---
+++ the-ceasar-cipher/ceasar_cipher.scala
@@ -0,0 +1,23 @@
+object Ceasar_chiper{
+	
+	def main(args:Array[String]) = {
+
+		var letters  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		var userInput = scala.io.StdIn.readLine();
+		userInput.foreach{i=>print(E(i, 3, letters))};
+
+		var userInput = scala.io.StdIn.readLine();
+		userInput.foreach{i=>print(D(i, 3, letters))};
+	}
+
+	val E = (c:Char, key:Int, a:String)=> if(c != ' ') a((a.indexOf(c.toUpper)+key)%a.size) else {
+		" ";
+	 };
+
+
+	val D = (c:Char, key:Int, a:String)=> if(c != ' ') a((a.size+(a.indexOf(c.toUpper)-key))%a.size) else {
+		" ";
+	 };
+
+}
